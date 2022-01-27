6c517ba5978212b3081ff6b4db5ec2bcdff3691a
Oreste Alexander <oreste.alexander@protonmail.com>
2022-01-27 11:33:36 -0600

    another commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 UPLOAD_FOLDER = 'static/uploads'
 
 app = Flask(__name__)
-app.secret_key = "secret key"
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
