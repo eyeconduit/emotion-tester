ea1b786a9ff1c76bcaa8fb539eac2fdac007a2cf
Oreste Alexander <oreste.alexander@protonmail.com>
2022-01-27 10:42:55 -0600

    change app

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask
 
 UPLOAD_FOLDER = 'static/uploads'
 
-app = Flask("main")
-
+app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
